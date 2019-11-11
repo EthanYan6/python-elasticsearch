27033432fc7fda70bbe3b368d4a6c805e2cf9a36
Ethanyan <yanyuliang6@163.com>
2019-11-11 19:47:59 +0800

    添加文档说明与封装好的类文件

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # python-elasticsearch
 Using elasticsearch in python
+
+
+
+### 说明
+
+此脚本是为了工作中提高效率，封装的类。
+
+### 作用
+
+elasticsearch 存储数据，当服务器上没有索引时先创建索引，再插入数据；如果已存在此索引，那么直接插入数据。
+
+### 使用示例
+
+```python
+es = ES(data, index_name)
+es.router()
+```
+
+> data 为**字典**类型**文档**数据
+>
+> index_name 为**字符串**类型的**索引名**。
+
+### 注意
+
+具体说明可以查看代码中注释部分，可修改 mapping 设置，可以修改 elasticsearch 的主机地址，当然也可以更改文档的 id ，目前已当前处理文档的时间戳作为文档 id。
---
+++ insert_data.py
@@ -0,0 +1,84 @@
+import elasticsearch
+import time
+
+# es 相关操作
+class ES(object):
+    def __init__(self, data):
+        """
+        实例化 es 对象
+        :param data: 字典类型
+        """
+        self.es = elasticsearch.Elasticsearch(hosts="your hosts", http_auth=("account", "password"))
+        self.data = data
+
+    def buildmapping(self):
+        """
+        创建 mapping 并返回状态信息
+        :return: 无返回结果
+        """
+        # 构造 mapping
+        mapping = {
+            "settings": {
+                "index": {
+                # 刷新间隔
+                    "refresh_interval": "3s"
+                },
+                # 副本数
+                "number_of_replicas": "0",
+                # 切片（分布在每个节点上的切片）
+                "number_of_shards": "1"
+            },
+            "mappings": {
+                "_doc": {
+                    "dynamic_templates": [
+                        {
+                            "string_fields": {
+                                "match": "*",
+                                "match_mapping_type": "string",
+                                "mapping": {
+                                    "type": "keyword",
+                                    "norms": "false"
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        # 创建索引
+        R = self.es.indices.create(index='your index name', body=mapping)
+        print(R)
+
+    def insertdata(self):
+        """
+        1.id暂定为当前时间戳
+        2.插入文档到 es 中
+        :return: 无法返回结果
+        """
+        id = str(time.time())
+        self.es.create(index='your index name', body=self.data, id=id)
+
+    def router(self):
+        """
+        1.查询 es 中是否存在此索引
+        2.存在的话直接存储数据
+        3.不存在的话先创建索引再存储数据
+        :return: 无返回
+        """
+        try:
+            response = self.es.search(index='your index name')
+            if response:
+                print("此索引已经存在，开始存储数据...")
+                try:
+                    self.insertdata()
+                    print("存储完成")
+                except Exception as e:
+                    print(e)
+
+        except elasticsearch.exceptions.NotFoundError:
+            print("查无此索引，正在创建...")
+            self.buildmapping()
+            print("索引创建完成，开始存储数据...")
+            self.insertdata()
+            print("存储完成")
